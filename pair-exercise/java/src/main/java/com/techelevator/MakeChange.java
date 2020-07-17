46a16ad24807838b7bbd3c7eb523867132878059
Greg Miller <gregam22@gmail.com>
2020-07-17 15:25:26 -0400

    Finished Get Change Exercise

--- pair-exercise/java/src/main/java/com/techelevator/MakeChange.java
+++ pair-exercise/java/src/main/java/com/techelevator/MakeChange.java
@@ -1,5 +1,7 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
 /*
  Write a command line program which prompts the user for the total bill, and the amount tendered. It should then
  display the change required.
@@ -12,7 +14,28 @@ package com.techelevator;
 public class MakeChange {
 
 	public static void main(String[] args) {
-
+		
+		Scanner userInput = new Scanner(System.in);
+		
+		
+		// asking for bill amt
+		System.out.println("What was your total bill?");
+		String totalBill = userInput.nextLine();
+		// Need to convert String to Double
+		double billDouble = Double.parseDouble(totalBill);
+		
+		// asking for amount paid with
+		System.out.println("What is the tender amount you are paying?");
+		String userPMT = userInput.nextLine();
+		// Need to convert String to Double
+		double pmtDouble = Double.parseDouble(userPMT);
+		
+		// need to declare change to be used in calc
+		double change = pmtDouble - billDouble;
+		
+		// subtract amout paid with from bill amt.
+		System.out.println("Your change is " + change);
+		
 	}
 
 }
